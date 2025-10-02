8535616178603bd3beef165140b39292179c4e5e
Waqar Ahmad <vizzahmadgmail.com>
2025-10-02 20:32:45 +0100

    add new feature - animated menu bar and bug fix

--- Foodie-Hamburger/script.js
+++ Foodie-Hamburger/script.js
@@ -2,11 +2,27 @@ const hamburgericon = document.querySelector('.hamburger-menu')
 const headercontent = document.querySelector('.header-content')
 const closeicon = document.querySelector('.close-icon')
 
+const nav = document.querySelector('nav')
+nav.addEventListener('click',(e)=>{
 
+
+})
 hamburgericon.addEventListener('click',(e)=>{
     headercontent.classList.add('menu-open')
-})
 
+})
+headercontent.addEventListener('click',(e)=>{
+e.stopPropagation()
+})
 closeicon.addEventListener('click',(e)=>{
     headercontent.classList.remove('menu-open')
+     
+})
+document.querySelector("#top > main > footer > h3").addEventListener('click',()=>{
+    document.querySelector("body > div").scrollTo(0,0)
+})
+
+window.addEventListener('click',(e)=>{
+    headercontent.classList.remove('menu-open')
+     
 })
