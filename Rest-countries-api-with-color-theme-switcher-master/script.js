51a1ed99fd2d53045994b778d29084caab590799
Waqar Ahmad <vizzahmadgmail.com>
2025-11-02 20:22:43 +0000

    adjust layout and add logic for filter by region

--- Rest-countries-api-with-color-theme-switcher-master/script.js
+++ Rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,11 +1,13 @@
 
-const countriesContainer = document.querySelector('.countries-container')
-
-
+let countriesContainer = document.querySelector('.countries-container')
+const filterByRegion = document.querySelector('.filter-by-region')
+let allCountriesData = 
 fetch ('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,subregion,currencies,tld,languages,borders')
 .then((res)=> res.json()).then((data)=>{
+
     data.forEach(country => {
         // console.log(country.capital[0]);
+       
 const countryCard = document.createElement('a')
 countryCard.classList.add('country-card')
 const x = country.population
@@ -35,3 +37,40 @@ countriesContainer.append(countryCard)
 
 })
 
+filterByRegion.addEventListener('change',(e)=>{
+console.log(filterByRegion.value);
+fetch (`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+.then((res)=> res.json()).then((data)=>{
+  countriesContainer = ''
+    data.forEach(country => {
+        // console.log(country.capital[0]);
+       
+const countryCard = document.createElement('a')
+countryCard.classList.add('country-card')
+const x = country.population
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+countryCard.href = `/country.html?name=${country.name.common}`
+const cardHTML = `
+
+  <img src="${country.flags.svg}" alt="flag">
+  <div class="card-text">
+    <h3 class="card-title">${country.name.common}</h3>
+    <p><b>Population</b>: ${numberWithCommas(x)}</p>
+    <p><b>Region</b>: ${country.region}</p>
+    <p><b>Capital</b>: ${country.capital[0]}</p>
+
+    </div>
+
+`
+countryCard.innerHTML = cardHTML
+
+countriesContainer.append(countryCard)
+
+        
+
+    });
+
+})
+})
