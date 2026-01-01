d0a23c483f9671207c55173556b1c775221d361e
Waqar Ahmad <vizzahmadgmail.com>
2026-01-01 19:59:42 +0000

    home page design done

---
+++ The-Green-Power-Clock/script.js
@@ -0,0 +1,10 @@
+// const submitEvent = document.getElementById('postcode-form')
+// const postCodeData = document.getElementById('postcode-input')
+
+// submitEvent.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+//     const postcode = postCodeData.value
+// console.log('hi', typeof postcode);
+
+// })
+// postCodeData.addEventListener
