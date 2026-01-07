f8be6d8091d798036e63fca65aaa215889bc5962
Waqar Ahmad <vizzahmadgmail.com>
2026-01-07 21:17:08 +0000

    feat: add UK grid carbon intensity calculation per fuel

--- The-Green-Power-Clock/script.js
+++ The-Green-Power-Clock/script.js
@@ -10,6 +10,18 @@ const recomandationUl = document.querySelector('.recomandation')
 const durationUl = document.querySelector('.duration')
 const indicatorH1Ul = document.querySelector('.indicatorH1')
 
+
+const emissionFactors = {
+   biomass: 120,
+  coal: 820,
+  imports: 300,   
+  nuclear: 0,
+  other: 300,
+  hydro: 0,
+  solar: 0,
+  wind: 0
+};
+
 submitEvent.addEventListener('submit',(e)=>{
     e.preventDefault()
     const postcode = postCodeData.value
@@ -19,7 +31,9 @@ const url = `https://api.carbonintensity.org.uk/regional/postcode/${p}`
 async function getApiData(){
     const res = await fetch(url)
     const data = await res.json()
-    // console.log(data.data[0].data[0].generationmix);
+    console.log(data.data[0].data[0].intensity);
+    console.log(data.data[0].data[0].generationmix);
+    const generationMixArray = data.data[0].data[0].generationmix
 const fromdate = data.data[0].data[0].from
 const toDate = data.data[0].data[0].to
 const foreCast = data.data[0].data[0].intensity.forecast
@@ -31,21 +45,26 @@ const diffInMs = end - start
 const diffInHours = diffInMs / (1000 * 60 * 60)
 durationUl.innerText = `on next ${diffInHours} hours`
 
-carbonText.innerText = `Current Carbon Level: ${foreCast}`
 
  indicatorH1Ul.innerText = index
 
+ const gc2o = generationMixArray.reduce((acc,item)=>{
+    const factor = emissionFactors[item.fuel] || 0
+    const contribution = (item.perc/100) * factor
+    return acc + contribution
 
+ },0)
+carbonText.innerText = `Grid Carbon Intensity for ${p} : ${gc2o.toFixed(1)} gCO₂/kWh`
 if(foreCast <= 150){
 
    
-    arrowUl.style.left = `${(540/540)*foreCast+10}px`
+    arrowUl.style.left = `90px`
     messageUl.innerText = 'Great time to use electricity!'
     recomandationUl.innerText = 'Run the dishwasher or charge the EV now.'
     
 }
 else if(foreCast > 150 && foreCast <=250){
-    arrowUl.style.left = `${(540/540)*foreCast}px`
+    arrowUl.style.left = `270px`
     messageUl.innerText = 'Think before you plug in.'
     recomandationUl.innerText = 'Its an average day; maybe wait for a sunnier/windier hour.'
     
@@ -69,4 +88,3 @@ refreshUl.addEventListener('click',()=>{
 container.style.display = 'block'
 resultsContainerUl.style.display = 'none'
 })
-
